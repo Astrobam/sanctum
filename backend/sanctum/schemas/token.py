ea49858ec8e66e3326fc3550190d0f25ff86bde6
Astrobam <itzrachin@gmail.com>
2025-09-03 01:13:43 +0530

    Create token.py

---
+++ backend/sanctum/schemas/token.py
@@ -0,0 +1,11 @@
+# backend/sanctum/schemas/token.py
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[EmailStr] = None
